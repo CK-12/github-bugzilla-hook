371cd31aad13ed16a6fe1b3e039194e61098008b
nimish <nimish@ck12.org>
2018-03-06 15:22:09 -0800

    - Added some changes to support bug actions as well as better parsing.

--- wsgi.py
+++ wsgi.py
@@ -40,6 +40,8 @@ HOME_DIR = '/var/www/github-bugzilla-hook'
 sys.path.insert(0, HOME_DIR)
 from settings import setEnv
 setEnv()
+
+FORCE_COMMENT = False
     
 
 def application(environ, start_response):
@@ -85,7 +87,7 @@ def application(environ, start_response):
     # Read the post data
     # Errors will be automatically converted to a 500
     post_data = environ['wsgi.input'].read(content_length)
-    print("post_data: %s" % post_data)
+    #print("post_data: %s" % post_data)
 
     # If a secret was set, validate the post data
     if 'GHBH_GITHUB_SECRET' in os.environ:
@@ -157,49 +159,49 @@ def get_bugs(data):
         https://developer.github.com/v3/activity/events/types/#pushevent
     """
 
+    supported_cmds = {'fix':        'Fixed',
+                      'fixed':      'Fixed',
+                      'fixes':      'Fixed',
+                      'addresses':  'Refers',
+                      're':         'Refers',
+                      'references': 'Refers',
+                      'refs':       'Refers',
+                      'refer':      'Refers',
+                      'refers':     'Refers',
+                      'reopens':    'Reopens',
+                      'reopen':     'Reopens'}
+
     bugs = {}
+
+    #build re to look for reference to a bug
+    bug_prefix = '(?:#|(?:Bug|Bug:|bug|bug:|BUG|BUG:)[: ]?)'
+    bug_reference = bug_prefix + '[0-9]+'
+    bug_command =  (r'(?P<action>[A-Za-z]*).?'
+                       '(?P<ticket>%s(?:(?:[, &]*|[ ]?and[ ]?)%s)*)' %
+                       (bug_reference, bug_reference))
+    command_re = re.compile(bug_command)
+    bug_re = re.compile(bug_prefix + '([0-9]+)')
+
+    #find all references to bugzilla bugs
 
     for commit in data["commits"]:
         sha = commit["id"]
         message = commit["message"]
         summary = message.split("\n")[0].strip()
         body = message.split("\n")[1:]
+        msg = summary + " ".join(body)
+	cmd_groups = command_re.findall(msg)
 
-        # look for a bug in the summary line
-        m = re.search(r"\(#\d+(\,.*)*\)", summary)
-        if m:
-            bugstr = summary[m.start()+2:m.end()-1]
-
-            bug = ''
-            for c in bugstr:
-                if c.isdigit():
-                    bug += c
-                else:
-                    break
-
+        for command, bugstrs in cmd_groups:
+            print("Processing command[%s], bugs[%s]" % (command, bugstrs))
+            bug = bug_re.findall(bugstrs)[0]
+            action = supported_cmds.get(command.lower(),'')
             if bug not in bugs:
-                bugs[bug] = [commit]
+                bugs[bug] = [(commit, action)]
             else:
-                bugs[bug].append(commit)
+                bugs[bug].append((commit, action))
 
-        # look for bugs in the message body
-        for bodyline in body:
-            bodyline = bodyline.strip()
-            m = re.match(r"^(Resolves|Related|Conflicts|Fixes|Fixed):\ +rhbz#\d+.*$", bodyline)
-
-            if not m:
-                continue
-
-            actionre = re.search("(Resolves|Related|Conflicts|Fixes|Fixed)", bodyline)
-            bugre = re.search(r"\d+", bodyline)
-            if actionre and bugre:
-                action = actionre.group()
-                bug = bugre.group()
-
-                if bug not in bugs:
-                    bugs[bug] = [commit]
-                else:
-                    bugs[bug].append(commit)
+        print("bugs after body: [%s]" % str(bugs))
 
     return bugs
 
@@ -216,7 +218,7 @@ def get_comments(data):
     comments = {}
 
     for bug in bugs.keys():
-        for commit in bugs[bug]:
+        for commit, action in bugs[bug]:
             comment = """
 %s
 Branch: %s
@@ -231,10 +233,11 @@ Date:   %s
         indent(commit["message"])
     )
             if not bug in comments:
-                comments[bug] = comment
+                comments[bug] = (comment, action)
             else:
-                comments[bug] += comment
+                comments[bug][0] += comment
 
+    print("comments: [%s]" % comments)
     return comments
 
 def post_to_bugzilla(bz, data):
@@ -245,7 +248,8 @@ def post_to_bugzilla(bz, data):
     posts = 0
 
     for bug_id in comments.keys():
-        text = comments[bug_id].strip()
+        text = comments[bug_id][0].strip()
+        action = comments[bug_id][1]
         has_comment = False
 
         # search by commits for particular branches
@@ -264,8 +268,17 @@ def post_to_bugzilla(bz, data):
                 has_comment = True
                 break
 
-        if not has_comment:
-            bz.update_bugs(bug_id, {'comment': {'comment': text, 'is_private': False}})
+        if not has_comment or FORCE_COMMENT:
+	    updateParams = bz.build_update(comment=text) 
+            if action == 'Fixed':
+                updateParams['status'] = 'RESOLVED'
+                updateParams['resolution'] = 'FIXED'
+            elif action == 'Reopens':
+                updateParams['status'] = 'REOPENED'
+            else:
+                print("No action found in the commit message.")
+            print("Update params: %s" % updateParams)
+            bz.update_bugs(bug_id, updateParams)
             posts += 1
 
     return posts
